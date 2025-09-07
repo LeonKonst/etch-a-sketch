5ee9afe7f26df8557eec748539b703c46767ccf8
Leontiadis Konstantinos <113901432+LeonKonst@users.noreply.github.com>
2025-09-07 14:52:18 +0200

    Create the sketch grid

---
+++ script.js
@@ -0,0 +1,16 @@
+const boardContainer = document.querySelector("#board-container");
+
+let numberOfRows = 16;
+let numberOfTilesPerRow = 16;
+
+for(let i = 0; i < numberOfRows; i++){
+    let rowContainer = document.createElement("div");
+    rowContainer.classList.add("grid-row");
+    boardContainer.appendChild(rowContainer);
+    for(let i = 0; i < numberOfTilesPerRow; i++){
+        let tile = document.createElement("div");
+        tile.classList.add("sketch-tile");
+        rowContainer.appendChild(tile);
+    }
+}
+
